272021ad723020f053e35bdb22b49a87c16a55c0
programmer-kabir <programmerkabirr@gmail.com>
2025-09-01 23:11:51 +0600

    sign up and sign in page create

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto  } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"], 
 });
 
 export const metadata = {
@@ -19,9 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        <body className={roboto.className}>
+      {/* <div>
+        <ul>
+          <li>
+            <Link href="signup">Create New Account</Link>
+          </li>
+          <li></li>
+        </ul>
+      </div> */}
         {children}
       </body>
     </html>
